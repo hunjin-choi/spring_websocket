2f75019d1fc534ab87594ebb6a832c3379c542e1
huchoi <nyz1058@nate.com>
2023-03-14 22:21:27 +0900

    stomp 도입해서 일부 코드 수정 MVC 도입

--- src/main/java/websocket/spring_websocket/chat/ChatController.java
+++ src/main/java/websocket/spring_websocket/chat/ChatController.java
@@ -1,26 +1,23 @@
 package websocket.spring_websocket.chat;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import websocket.spring_websocket.chat.dto.ChatMessage;
 import websocket.spring_websocket.chat.dto.ChatRoom;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
-
-    private final ChatService chatService;
-
-    //
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    private final SimpMessageSendingOperations messagingTemplate;
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
